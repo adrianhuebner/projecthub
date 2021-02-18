1dfbf5bdf8e3419aabde37b53ab3e171760343db
adrianhuebner <adrianh92@gmail.com>
2021-02-18 13:38:50 -0800

    got form started with user being able to add their username

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -4,8 +4,20 @@ import React from 'react'
 
 function SignUpForm() {
   return (
-    <div>
-      Hello
+    <div className="form-content">
+      <form className="form">
+        <h1>Create an account with us to help you keep track of all your projects!</h1>
+        <h2>Please fill out the form below to your account created!</h2>
+        <div className="form-input">
+          <label htmlFor="username">
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your username here"
+            />
+          </label>
+        </div>
+      </form>
     </div>
   )
 }
