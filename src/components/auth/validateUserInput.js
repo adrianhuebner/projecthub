9f22f7eef4919f530cfb67ffc7c191fd03e6ede9
adrianhuebner <adrianh92@gmail.com>
2021-03-04 14:00:55 -0800

    started username and email validation with regex added for checking an email

--- src/components/auth/validateUserInput.js
+++ src/components/auth/validateUserInput.js
@@ -1,2 +1,27 @@
 'use strict';
 
+export default function validateFormErrors(formValues){
+
+  let errors = {};
+  
+  // Username Errors
+  if(!formValues.username.trim()){
+    errors.username = "Please enter an username"
+  }
+
+  // Email Errors
+  let emailRegex = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+
+  if(!formValues.email){
+    errors.email = "Please enter an email"
+  }
+
+  else if(!emailRegex){
+    errors.email = "Please enter a valid email address"
+  }
+
+  // Password Errors
+  if(!formValues.password){
+    errors.password = "Please enter a password"
+  }
+}
